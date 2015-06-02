a9e90fb8b3228b9498d8c39ee7667b5371e90edb
ronen <narkisr@gmail.com>
2015-06-02 16:23:11 +0300

    using puppet ssh auth key

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,10 +1,7 @@
-group{ 'puppet': ensure  => present }
-
 node default {
-  class{'supernal':
-    user    => 'vagrant',
-    appname => 'web'
+  ssh_authorized_key { 'ronen@lepus':
+    user => 'vagrant',
+    type => 'ssh-rsa',
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDmp4knkVGw92Ok6kN95NxjjcRBbHttWX8NbA1a00hkYN1eva5uKJb0CqRukb3a/m2AuKPTn5ShDGHRwyd9TrBqgPc0ee5rFaNF3rGKsth3t/CrvnSU0dpMGUzAYy+HEeutEdPCaDDyZtP6rImtCq6KfOh8NZ4hqRwTvyraNI8fYhC/ZyGdT6sMxedEV9azar3gKahPWzuuIDmQS5ZCS2sHWrbAQODhTnuzHuQR+Toafwba1u1YtNGIi/Ro/xraVXlvOCOI75DhhMrxGE6/ruBHgorgCHwwJl+JZOInpQvtWXM+vV7GUYW2AiVIcJg+XCt12YN3Zkyk0F6G3DPujU51',
   }
-
-  include ssh
 }
